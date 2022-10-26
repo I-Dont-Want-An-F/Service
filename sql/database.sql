f80111c0917d43a5bb8197410301bc96b9aea8a1
dylanmount <dcm22@calvin.edu>
2022-10-25 22:32:49 -0400

    added queries

--- sql/database.sql
+++ sql/database.sql
@@ -1,87 +1,94 @@
---Database designed by Dylan and Einstein 
+  --Database designed by Dylan 
 -- 
 
 DROP TABLE IF EXISTS reply;
 DROP TABLE IF EXISTS post;
-DROP TABLE IF EXISTS stats;
+DROP TABLE IF EXISTS rating;
 DROP TABLE IF EXISTS userclass;
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS class;
 
 -- Create the schema.
 CREATE TABLE class (
 	ID integer PRIMARY KEY,
-	name varchar(50),
-    subject varchar(50), 
+	longName varchar(50),
+    shortName varchar(10),
+    subject varchar(50)
 	);
 
-CREATE TABLE user (
+CREATE TABLE users (
 	ID integer PRIMARY KEY, 
+    name varchar(50),
     emailAddress varchar(50) NOT NULL,
-    teacher boolean
+    position varchar(20)
 	);
 
 CREATE TABLE userclass (
 	ID integer PRIMARY KEY, 
-    userID references user(ID),
-	classID  references class(ID),
-    --
+    userID integer references users(ID),
+	classID integer references class(ID)
 	);
 
-CREATE TABLE  stats (
+CREATE TABLE  rating (
 	 ID integer PRIMARY KEY,
-     classID references class(ID),
+     classID integer references class(ID),
+    stars integer, 
+    hw varchar(10)
      --need to finialze what stats we want 
 	);
 
 CREATE TABLE post(
     ID integer PRIMARY KEY, 
-    classID references clsss(ID),
+    classID integer references class(ID),
     question boolean,
-    user references user(ID),
+    userID integer references users(ID),
     date integer,
-    text varchar(200),
+    text varchar(200)
 );
 
 create table reply(
-ID integer PRIMARY KEY, 
-postID references post(ID),
-userID references user(ID),
-text varchar(200),
+    ID integer PRIMARY KEY, 
+    postID integer references post(ID),
+    userID integer references users(ID),
+    text varchar(200)
     );
 
 
 
 -- Allow users to select data from the tables.
 GRANT SELECT ON class TO PUBLIC;
-GRANT SELECT ON user TO PUBLIC;
+GRANT SELECT ON users TO PUBLIC;
 GRANT select ON userclass to PUBLIC;
-GRANT SELECT ON stats TO PUBLIC;
 GRANT SELECT ON post to PUBLIC; 
-GRANT SELECT ON reply to PUBLIC; 
+GRANT SELECT ON rating to PUBLIC; 
 
 
 -- Add sample records.
 
  --classes
-insert into class values (1, 'cs 112', 'cs');
-insert into class values(2, 'cs 262', 'cs');
-insert into class values(3, 'engr 220','cs');
+insert into class values (1,'intro to data structures', 'cs 112', 'cs');
+insert into class values(2,'web devolomnet', 'cs 262', 'cs');
+insert into class values(3,'computer engineering', 'engr 220','cs');
 
 --users
-insert into user values(1,abc12, false);
-insert into user values(2, def34, true);
+insert into users values(1,'john','abc12@calvin.edu', 'student');
+insert into users values(2, 'josh', 'def34@calvin.edu', 'professor');
 
 --userclass 
 insert into userclass values(1,1,1);
 insert into userclass values(2,1,2);
+insert into userclass values(3,2,2);
 
 --stats 
-insert into stats values(1,1);
+insert into rating values(1,1,3.2,'weekly');
 
 --post 
-insert into post values(1,1,true,1,11202222, 'is the proffesore helpful');
-insert into post values(2,2,false,1,'I really like working on the app');
+insert into post values(1,1,true,1,11202022, 'is the professor helpful');
+insert into post values(2,1,false,1,11202022, 'this class is boring');
+insert into post values(3,1,true,1,11202022, 'Should I make lab 1 easier');
+insert into post values(4,1, false,2,11202022, 'there is no textbook');
+insert into post values(5,2,false,1,10252022,'I really like working on the app');
+
 
 --reply
-insert into relpy values(1,1,2,'yes');
+insert into reply values(1,1,2,'yes');
--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,37 @@
 -- created by dylan mount
+
+--select professors that teach a certain class
+Select name from  users, userclass, class
+where users.position = 'professor' 
+and userclass.classID=class.ID 
+and userclass.userID=users.ID
+and class.shortName = 'cs 262';
+
+--select certain classes a student is taking
+Select shortName from  users, userclass, class
+where users.position = 'student' 
+and users.name = 'john'
+and userclass.classID=class.ID 
+and userclass.userID=users.ID;
+
+--select all post for a certain class
+select text from post, class
+where post.classID=class.ID
+and class.shortName='cs 112';
+
+--select all comments for a certain class 
+select text from post, class
+where post.classID=class.ID
+and class.shortName='cs 112'
+and post.question = false;
+
+--select all questions for a certain class
+select text from post, class
+where post.classID=class.ID
+and class.shortName='cs 112'
+and post.question = true;
+
+--select the ratings for a certain class 
+select stars, hw from rating, class
+where class.shortName = 'cs 112'
+and class.ID=rating.classID;
