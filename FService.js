db62699812f15932088102b71b15826f909f4bff
Einstein Essibu <einsteinewoof@gmail.com>
2022-11-11 10:54:43 -0500

    Post feature

--- FService.js
+++ FService.js
@@ -26,7 +26,14 @@ router.get("/comments/:id", comments);//selects all the comments for a given cla
 router.get("/questions/:id", questions); //selects all the questions for a given class
 router.get("/rating/:id", rating);// retuns a rating for a class 
 router.get("/classes", classes); //returns all classes 
-router.get("/reply/:id", reply); //returns the reply to a comment 
+router.get("/reply/:id", reply); //returns the reply to a comment
+
+router.post("/questions", createcomments); // creates a question created by a user
+router.put("/questions/:id", updatecomments); // update a question created by a user
+
+router.post("/reply", createreply); // creates a question created by a user
+router.put("/reply/:id", updatereply); // update a question created by a user
+
 
 
 app.use(router);
@@ -98,6 +105,7 @@ function classTook(req, res, next) {
     })
 }
 
+
 //selects all comments for a certain class 
 function comments(req, res, next) {
     db.many("select text,post.ID,users.username from post, class,users where post.classID=class.ID and class.shortName =${id} and post.question = false and post.userID=users.ID", req.params)
@@ -108,6 +116,28 @@ function comments(req, res, next) {
         next(err);
     })
 }
+
+function createcomments(req, res, next) {
+    db.many("insert into text,post.ID,users.username from post, class,users where post.classID=class.ID and class.shortName =${id} and post.question = false and post.userID=users.ID", req.params)
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        next(err);
+    })
+}
+
+function updatecomments(req, res, next) {
+    db.many("UPDATE text,post.ID,users.username from post, class,users where post.classID=class.ID and class.shortName =${id} and post.question = false and post.userID=users.ID", req.params)
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        next(err);
+    })
+}
+
+
 //selects all the questions for a certian class
 function questions(req, res, next) {
     db.many("select text,post.ID, users.username from post, users, class where post.classID=class.ID and class.shortName =${id} and post.question = true and post.userID=users.ID", req.params)
@@ -118,6 +148,7 @@ function questions(req, res, next) {
         next(err);
     })
 }
+
 
 //selects the rating for a certain class
 function rating(req, res, next) {
@@ -152,3 +183,24 @@ function reply(req, res, next) {
         next(err);
     })
 }
+
+function createreply(req, res, next) {
+    db.many("INSERT into * from reply where reply.postID= ${id}", req.params)
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        next(err);
+    })
+}
+
+function updatereply(req, res, next) {
+    db.many("UPDATE * from reply where reply.postID= ${id}", req.params)
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        next(err);
+    })
+}
+
