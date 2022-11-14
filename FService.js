16c5254de319b5f1892022b7b9e0ee254a636542
dylanmount <dcm22@calvin.edu>
2022-11-14 11:02:13 -0500

    $

--- FService.js
+++ FService.js
@@ -208,7 +208,7 @@ function updatereply(req, res, next) {
 
 
 function messageRooms(req, res, next){
-    db.many("select id, userOne, userTwo from messagerooms where (userOne = {id} OR userTwo = {id})", req.params)
+    db.many("select id, userOne, userTwo from messagerooms where (userOne = ${id} OR userTwo = ${id})", req.params)
     .then(data => {
         res.send(data);
     })
@@ -219,7 +219,7 @@ function messageRooms(req, res, next){
 
 
 function messages(req, res, next){
-    db.many("select * from messages where roomID = {id}", req.params)
+    db.many("select * from messages where roomID = ${id}", req.params)
     .then(data => {
         res.send(data);
     })
